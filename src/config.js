95179adace8b173380b89a4a68409806a66b2038
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-11-18 21:14:53 +0100

    configure amplify

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "sasil-amormat-uploads"
+    },
+    // apiGateway: { //TODO :// edit the identity pool dashboard to add lambda api
+    //   REGION: "us-east-1",
+    //   URL: "https://vs74jlacy5.execute-api.us-east-1.amazonaws.com/dev"
+    // },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_C3v27VkJ5",
+      APP_CLIENT_ID: "2082gulojtdlsaqo5qekoml7vt",
+      IDENTITY_POOL_ID: "us-east-1:7ed5deda-6efb-4031-b136-52641499b71e"
+    }
+  };
